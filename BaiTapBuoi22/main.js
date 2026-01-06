1a60e3d4b2e1050153f30289edec131c55c89710
buihuythanh200-code <buihuythanh200@gmail.com>
2026-01-06 14:18:33 +0700

    fix error 6/1/2026

--- BaiTapBuoi22/main.js
+++ BaiTapBuoi22/main.js
@@ -1,8 +1,8 @@
 //Lession 1: Academic performance classification
-function studentClassfication(score) {
+function getStudentLevel(score) {
   if (score >= 0 && score <= 10) {
     if (score >= 9 && score <= 10) {
-      return "Excelent";
+      return "Excellent";
     } else if (score >= 8 && score < 9) {
       return "Very Good";
     } else if (score >= 6.5 && score < 8) {
@@ -16,89 +16,77 @@ function studentClassfication(score) {
     return "Error! Please re-enter";
   }
 }
-// test studentClassfication
-console.log(studentClassfication(11));
-console.log(studentClassfication(9));
-console.log(studentClassfication(8.5));
-console.log(studentClassfication(6.7));
-console.log(studentClassfication(5));
-console.log(studentClassfication(3));
+// test getStudentLevel
+console.log(getStudentLevel(11));
+console.log(getStudentLevel(9));
+console.log(getStudentLevel(8.5));
+console.log(getStudentLevel(6.7));
+console.log(getStudentLevel(5));
+console.log(getStudentLevel(3));
 
 //Lesson2: Days in a Month Calculator
-let month = 2;
-switch (month) {
-  case 1:
-    console.log(`Month ${month} has 31 days`);
-    break;
-  case 2:
-    console.log(`Month ${month} has 28 days`);
-    break;
-  case 3:
-    console.log(`Month ${month} has 31 days`);
-    break;
-  case 4:
-    console.log(`Month ${month} has 30 days`);
-    break;
-  case 5:
-    console.log(`Month ${month} has 31 days`);
-    break;
-  case 6:
-    console.log(`Month ${month} has 30 days`);
-    break;
-  case 7:
-    console.log(`Month ${month} has 31 days`);
-    break;
-  case 8:
-    console.log(`Month ${month} has 31 days`);
-    break;
-  case 9:
-    console.log(`Month ${month} has 30 days`);
-    break;
-  case 10:
-    console.log(`Month ${month} has 31 days`);
-    break;
-  case 11:
-    console.log(`Month ${month} has 30 days`);
-    break;
-  case 12:
-    console.log(`Month ${month} has 31 days`);
-    break;
+function getDaysInMonth(month) {
+  switch (month) {
+    case 1:
+    case 3:
+    case 5:
+    case 7:
+    case 8:
+    case 10:
+    case 12:
+      return 31;
+
+    case 4:
+    case 6:
+    case 9:
+    case 11:
+      return 30;
+
+    case 2:
+      return 28;
+    default:
+      return "Error!";
+  }
 }
+console.log("Month has: " + getDaysInMonth(3) + " days");
 
 //Lesson 3: Even/Odd Check
-let number = 4;
-let result = number % 2 === 0 ? "Even" : "Odd";
-console.log("The number is " + result);
+function isEvenOrOdd(number) {
+  let result = number % 2 === 0 ? "Even" : "Odd";
+  return result;
+}
+console.log("The number is " + isEvenOrOdd(5));
 //Lesson 4: Movie Ticket Price Calculation
-let age = 12;
-let price = age < 13 ? 0.5 * 100000 : 100000;
-console.log("The price is: " + price);
+function calcTicketPrice(age) {
+  let price = age < 13 ? 0.5 * 100000 : 100000;
+  return price;
+}
+console.log("The price is: " + calcTicketPrice(12));
 
 //Lesson 5: Temperature Conversion
-function convertTemperature(celsius) {
+function toFahrenheit(celsius) {
   let result = celsius * 1.8 + 32;
   return result;
 }
-console.log(convertTemperature(32));
+console.log(toFahrenheit(32));
 
 //Lesson 6:Household Electricity Bill Calculation
 
-function caculateBillElectricity(consumption) {
+function calcBillElectricity(consumption) {
   let price = 0;
   if (consumption <= 50) {
     price = 1678 * consumption;
     return price;
   } else if (consumption >= 51 && consumption <= 100) {
-    price = 1678 * 50 + 1734 * ((consumption - 51) / 1 + 1);
+    price = 1678 * 50 + 1734 * (consumption - 50);
     return price;
   } else if (consumption >= 101 && consumption <= 200) {
-    price = 1678 * 50 + 1734 * 50 + 2014 * ((consumption - 101) / 1 + 1);
+    price = 1678 * 50 + 1734 * 50 + 2014 * (consumption - 100);
     return price;
   } else {
-    price =
-      1678 * 50 + 1734 * 50 + 2014 * 100 + 2536 * ((consumption - 201) / 1 + 1);
+    price = 1678 * 50 + 1734 * 50 + 2014 * 100 + 2536 * (consumption - 200);
     return price;
   }
 }
 
-console.log("Price of Bill Electricity: " + caculateBillElectricity(205));
+console.log("Price of Bill Electricity: " + calcBillElectricity(205));
